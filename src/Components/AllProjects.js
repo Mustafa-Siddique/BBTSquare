25bffbd8fc2e2ddec1e5282b37710d188ef5f493
Mustafa-Siddique <66897095+Mustafa-Siddique@users.noreply.github.com>
2022-03-28 23:09:35 +0530

    array cards created

--- src/Components/AllProjects.js
+++ src/Components/AllProjects.js
@@ -34,23 +34,24 @@ export default function AllProjects() {
   ]
 
   const renderProjects = (arr, index) => {
-    <div className="col-md-4" key={index}>
-      <div className="card" style="width: 18rem;">
+    return(
+    <div className="col-md-6 my-2" key={index}>
+      <div className="card" style={{minWidth: "18rem", borderRadius: "25px"}}>
         <div className="card-body">
-          <h5 className="card-title text-danger">{console.log(arr.name)}</h5>
+          <h5 className="card-title text-danger">{arr.name}</h5>
           <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="card-link">Card link</a>
-          <a href="#" className="card-link">Another link</a>
+          <a href="#" className="btn btn-outline-primary">Card link</a>
         </div>
       </div>
     </div>
+    )
   }
 
   return (
     <div className='allProjects'>
       <h2>All Projects</h2>
-      <div className="row">
+      <div className="row my-5">
         {arr.map(renderProjects)}
       </div>
     </div>
